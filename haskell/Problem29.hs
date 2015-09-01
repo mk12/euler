6c935cf406ae1cda3882ae3908279b4cfb90b099
Mitchell Kember <mk12360@gmail.com>
2015-09-01 16:20:48 -0400

    Solve problem 29 in Haskell

---
+++ haskell/Problem29.hs
@@ -0,0 +1,10 @@
+-- Copyright 2015 Mitchell Kember. Subject to the MIT License.
+-- Project Euler: Problem 29
+-- Distinct powers
+
+module Problem29 where
+
+import Data.List (nub)
+
+solve :: Int
+solve = length . nub $ [a^b | a <- range, b <- range] where range = [2..100]
