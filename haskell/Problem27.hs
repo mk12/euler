8365e057e5743925d1d71c0cd0f1c5b26544da83
Mitchell Kember <mk12360@gmail.com>
2015-08-31 21:52:16 -0400

    Solve problem 27 in Haskell

---
+++ haskell/Problem27.hs
@@ -0,0 +1,18 @@
+-- Copyright 2015 Mitchell Kember. Subject to the MIT License.
+-- Project Euler: Problem 27
+-- Quadratic primes
+
+module Problem27 where
+
+import Common (isPrime, maximumOn)
+
+numQuadPrimes :: (Int, Int) -> Int
+numQuadPrimes (a, b) = length . takeWhile works $ [0..]
+  where
+    works n = isPrime $ n^2 + a * n + b
+
+solve :: Int
+solve = mult . maximumOn numQuadPrimes $ [(a, b) | a <- range, b <- range]
+  where
+    range = [-999..999]
+    mult = uncurry (*)
