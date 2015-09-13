a53dc8ef758d63df80eb5ea02316fedb003d5fcc
Mitchell Kember <mk12360@gmail.com>
2015-09-13 13:04:10 -0400

    Solve problem 41 in Haskell

---
+++ haskell/Problem41.hs
@@ -0,0 +1,15 @@
+-- Copyright 2015 Mitchell Kember. Subject to the MIT License.
+-- Project Euler: Problem 41
+-- Pandigital prime
+
+module Problem41 where
+
+import Common (digits, isPrime)
+
+import Data.List (sort)
+
+solve :: Int
+solve =  maximum . filter works . takeWhile (<= 7654321) $ potentials
+  where
+    potentials = 7000001 : 7000003 : map (+ 6) potentials
+    works n = sort (digits n) == [1..7] && isPrime n
