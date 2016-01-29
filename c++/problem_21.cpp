a42c26ca26aed2ab878d870f3a6c67b9ea4b146d
Mitchell Kember <mk12360@gmail.com>
2016-01-29 01:06:52 -0800

    Solve problem 21 in C++

---
+++ c++/problem_21.cpp
@@ -0,0 +1,31 @@
+// Copyright 2016 Mitchell Kember. Subject to the MIT License.
+// Project Euler: Problem 21
+// Amicable numbers
+
+#include "common.hpp"
+
+#include <array>
+
+namespace problem_21 {
+
+long solve() {
+	typedef std::vector<long>::size_type sz_t;
+
+	constexpr sz_t max = 10000;
+	std::vector<long> sums(max);
+	for (sz_t i = 1; i < max; ++i) {
+		sums[i] = common::sum_proper_divisors(static_cast<long>(i));
+	}
+
+	long total = 0;
+	for (sz_t i = 1; i < max; ++i) {
+		sz_t si = static_cast<sz_t>(sums[i]);
+		if (i < si && si < max && static_cast<sz_t>(sums[si]) == i) {
+			total += i;
+			total += si;
+		}
+	}
+	return total;
+}
+
+} // namespace problem_21
